ab53f477c27a48d146a8726652094b7bf9c992ff
Vorleak Yek <vorleakyek@gmail.com>
2026-03-27 21:19:51 -0700

    Add bookCard and render the books

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
 import HeroSection from "@/components/HeroSection";
+import { sampleBooks } from "@/lib/constants";
+import BookCard from "@/components/BookCard";
 
 export default function Page() {
   return (
     <main className="wrapper container">
       <HeroSection />
+
+      <div className="library-books-grid">
+        {sampleBooks.map((book) => (
+          <BookCard
+            key={book._id}
+            title={book.title}
+            author={book.author}
+            coverURL={book.coverURL}
+            slug={book.slug}
+          />
+        ))}
+      </div>
     </main>
   );
 }
